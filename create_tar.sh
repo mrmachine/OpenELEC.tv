1eda4a40e77abf35b2620b22e4cb065d24341fce
Dale Hamel <dale.hamel@srvthe.net>
2013-02-17 13:02:04 -0600

    Python support working

--- create_tar.sh
+++ create_tar.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git archive --format=tar --prefix=rasplex-source-$1/ tags/$1 | bzip2 > rasplex.tar.bz2
+git archive --format=tar --prefix=OpenELEC-source-$1/ tags/$1 | bzip2 > OpenELEC-source-$1.tar.bz2
